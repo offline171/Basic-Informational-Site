da2671c34bae67d2e4c2b23cea28dfeafb0030c0
offline171 <offline.stein@gmail.com>
2025-05-11 22:12:15 -0400

    Solution
    Lifted changes from community solution admittedly, but it was good to see the differences and find out what I did wrong. At least I tried to make it work on my own first.

--- main.js
+++ main.js
@@ -2,16 +2,26 @@ var http = require('http');
 var url = require('url');
 var fs = require('fs');
 
+const page404 = fs.readFileSync("404.html", "utf-8", (err,data) => {
+  if (err) throw err;
+  return data;
+});
+
 http.createServer(function (req, res) {
   var q = url.parse(req.url, true);
   var filename = "." + q.pathname + ".html";
+  if(q.pathname === "/"){
+    filename = "./index.html";
+  }
   fs.readFile(filename, function(err, data) {
     if (err) {  
       res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found " + filename);
+      res.write(page404);
+      return res.end();
     } 
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     return res.end();
   });
 }).listen(8080);
+
